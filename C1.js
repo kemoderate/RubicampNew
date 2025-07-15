a4a6a280fbdc736677a22e6ae9b6cbbea93a252a
kemoderate <fahminomana@gmail.com>
2025-07-15 20:46:57 +0700

    challenge1selesai

--- C1.js
+++ C1.js
@@ -1,1 +1,10 @@
-function
+function sum(...args) {
+    const hasil = args.reduce((total, num) => total + num, 0);
+    console.log(hasil);
+    return hasil;
+}
+
+sum(1, 2, 7);         
+sum(1, 4);            
+sum(11);              
+sum(10, 3, 6, 7, 9);  
