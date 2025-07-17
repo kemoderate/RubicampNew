49cefbe30982b20a6598e4026a67004fdc58fcdb
kemoderate <fahminomana@gmail.com>
2025-07-17 14:31:49 +0700

    C8 selesai

--- C8.js
+++ C8.js
@@ -1,7 +1,20 @@
 function pola(str){
+    for (let i = 0; i <= 9; i++) {  
+        for( let j = 0; j <= 9; j++){
+         let testStr = str.replace("#",i).replace("#",j);
+       
+       let [leftSide,rightSide] = testStr.split("=");
+       let hasilKiri = eval(leftSide);
+       let hasilKanan = Number(rightSide);
 
+         if (hasilKiri === hasilKanan){
+        return [i,j]
+     
+                }
+            }
+       } 
 }
-console.log(pola("42#3 * 188 - 80#204"))
+console.log(pola("42#3 * 188 = 80#204"))
 //result :[8,5]
 console.log(pola("8#61 * 895 = 78410#5"))
 // result [7,9]
