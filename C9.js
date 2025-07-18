09bb839ff83290ed17db30e3822249e888a8625f
kemoderate <fahminomana@gmail.com>
2025-07-18 10:30:02 +0700

    matriks beres

--- C9.js
+++ C9.js
@@ -1,7 +1,29 @@
 function spiral(param1) {
     
+    let matriks = []
+    let counter = 0;
+
+    for (let i = 0; i < param1 ; i++) { // Outer loop
+        let row = [];
+  for (let j = 0; j < param1; j++) { // Inner loop
+    row.push(counter);
+    counter++;
+  }
+  matriks.push(row);
+}
+console.log(matriks)
+
 }
 
 spiral(5)
 spiral(6)
 spiral(7)
+// expected output 
+// // spiral(5) 
+// 0,1,2,3,4
+// 5,6,7,8,9
+// 10,11,12,13,14
+// 15,16,17,18,19
+// 20,21,22,23,24
+// tampilan result nya:
+// [0,1,2,3,4,9,14,19,24,23,22,21,20,15,10,5,6,7,8,13,18,17,16,11,12]
