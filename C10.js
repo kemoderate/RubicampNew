95ddae8e2d682edbf61689ebe7f1b4923ff275d8
kemoderate <fahminomana@gmail.com>
2025-07-21 19:02:20 +0700

    proses pemindahan C6 ke C10

--- C10.js
+++ C10.js
@@ -1,1 +1,25 @@
+const readline = require ('readline');
 
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+})
+
+// // function sentenceManipulation(sentence){
+//     let arrayKata = sentence.toLowerCase().split(" ");
+    
+//     let vokal = ['a', 'i', 'u', 'e', 'o'];
+//     for (let i = 0; i < arrayKata.length; i++){
+//         sentence = arrayKata[i];
+ 
+//     }
+
+//  let hurufPertama = sentence[0].toLowerCase();
+//     if (vokal.includes(hurufPertama)) {
+//         console.log(sentence);
+//     } else {
+//         let sisasentence = sentence.slice(1);
+//         let hasil = sisasentence + hurufPertama + 'nyo';
+//         console.log(hasil);
+//     }
+// }
