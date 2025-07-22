f7e34b98b8f160fd9ba71426b5fa1437cbecb67d
kemoderate <fahminomana@gmail.com>
2025-07-22 09:38:21 +0700

    challenge 10 selesai

--- C10.js
+++ C10.js
@@ -1,30 +1,39 @@
 const readline = require('readline');
-
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'tulis kalimatmu disini > '
 });
 
+function sentenceManipulation(sentence) {
+  const vokal = ['a', 'i', 'u', 'e', 'o'];
+  const kataArray = sentence.split(" ");
+  const hasil = [];
 
-rl.on('line', function (sentence) {
-    let arrayKata = sentence.toLowerCase().split(" ");
+  for (let i = 0; i < kataArray.length; i++) {
+    const kata = kataArray[i];
+    const hurufAwal = kata.charAt(0).toLowerCase();
 
-    let vokal = ['a', 'i', 'u', 'e', 'o'];
-    for (let i = 0; i < arrayKata.length; i++) {
-        sentence = arrayKata[i];
-
-    }
-
-    let hurufPertama = sentence[0].toLowerCase();
-    if (vokal.includes(hurufPertama)) {
-        console.log(sentence);
+    if (vokal.includes(hurufAwal)) {
+      hasil.push(kata);
     } else {
-        let sisasentence = sentence.slice(1);
-        let hasil = sisasentence + hurufPertama + 'nyo';
-        console.log(hasil);
+      const konversi = kata.slice(1) + hurufAwal + 'nyo';
+      hasil.push(konversi);
     }
+  }
 
+  console.log(`dan output dengan hasil konversi: ${hasil.join(" ")}`);
+  rl.prompt();
+}
+
+console.log("Ketik 'exit' untuk keluar dari program.");
+rl.prompt();
+
+rl.on('line', (input) => {
+  if (input.toLowerCase() === 'exit') {
+    console.log('Good bye!');
     rl.close();
+  } else {
+    sentenceManipulation(input);
+  }
 });
-
-
