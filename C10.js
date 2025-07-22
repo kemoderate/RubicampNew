2c024f7e8c4e2309fc06bca6b1a679fec02e98ae
kemoderate <fahminomana@gmail.com>
2025-07-22 09:28:37 +0700

    proses C10

--- C10.js
+++ C10.js
@@ -1,25 +1,30 @@
-const readline = require ('readline');
+const readline = require('readline');
 
 const rl = readline.createInterface({
-    input : process.stdin,
-    output : process.stdout
-})
+    input: process.stdin,
+    output: process.stdout
+});
 
-// // function sentenceManipulation(sentence){
-//     let arrayKata = sentence.toLowerCase().split(" ");
-    
-//     let vokal = ['a', 'i', 'u', 'e', 'o'];
-//     for (let i = 0; i < arrayKata.length; i++){
-//         sentence = arrayKata[i];
- 
-//     }
 
-//  let hurufPertama = sentence[0].toLowerCase();
-//     if (vokal.includes(hurufPertama)) {
-//         console.log(sentence);
-//     } else {
-//         let sisasentence = sentence.slice(1);
-//         let hasil = sisasentence + hurufPertama + 'nyo';
-//         console.log(hasil);
-//     }
-// }
+rl.on('line', function (sentence) {
+    let arrayKata = sentence.toLowerCase().split(" ");
+
+    let vokal = ['a', 'i', 'u', 'e', 'o'];
+    for (let i = 0; i < arrayKata.length; i++) {
+        sentence = arrayKata[i];
+
+    }
+
+    let hurufPertama = sentence[0].toLowerCase();
+    if (vokal.includes(hurufPertama)) {
+        console.log(sentence);
+    } else {
+        let sisasentence = sentence.slice(1);
+        let hasil = sisasentence + hurufPertama + 'nyo';
+        console.log(hasil);
+    }
+
+    rl.close();
+});
+
+
