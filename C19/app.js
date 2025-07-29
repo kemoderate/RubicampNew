3e4e1950ffd85282b1e30f969ae454e9bb5d76f1
kemoderate <fahminomana@gmail.com>
2025-07-29 16:22:18 +0700

    edit dan delete beres

--- C19/app.js
+++ C19/app.js
@@ -14,55 +14,75 @@ app.use(express.static('public'));
 
 // Helper untuk baca/tulis file
 function loadData() {
-  return JSON.parse(fs.readFileSync('data.json'));
+    return JSON.parse(fs.readFileSync('data.json'));
 }
 function saveData(data) {
-  fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
+    fs.writeFileSync('data.json', JSON.stringify(data, null, 2));
 }
 
 // READ
 app.get('/', (req, res) => {
-  const data = loadData();
-  res.render('index', { data });
+    const data = loadData();
+    res.render('index', { data });
 });
 
 // CREATE (Form)
 app.get('/add', (req, res) => {
-  res.render('form', { formTitle: '', formAction: '/add', item: null });
+    res.render('form', { formTitle: '', formAction: '/add', item: null, index: null });
 });
 
 app.post('/add', (req, res) => {
-  const data = loadData();
-  const newItem = {
-    name:  req.body.name,
-    height: parseInt(req.body.height),
-    weight: parseInt(req.body.weight),
-    birthdate: req.body.birthdate,
-    married: req.body.married === 'true'
-  }
-  data.push(newItem);
-  saveData(data);
-  res.redirect('/');
+    const data = loadData();
+    const newItem = {
+        name: req.body.name,
+        height: parseInt(req.body.height),
+        weight: parseInt(req.body.weight),
+        birthdate: req.body.birthdate,
+        married: req.body.married === 'true'
+    }
+    data.push(newItem);
+    saveData(data);
+    res.redirect('/');
 });
 
 // UPDATE (Form)
 app.get('/edit/:index', (req, res) => {
-  const data = loadData();
-  res.render('form', { item: data[req.params.index], index: req.params.index });
+    const data = loadData();
+    const index = parseInt(req.params.index) - 1;
+    const person = data[index];
+    res.render('form', {
+        formTitle: 'Edit',
+        formAction: `/edit/${index}`,
+        item: person,
+        index
+    });
 });
+
 app.post('/edit/:index', (req, res) => {
-  const data = loadData();
-  data[req.params.index] = req.body;
-  saveData(data);
-  res.redirect('/');
+    const data = loadData();
+    const index = parseInt(req.params.index) - 1;
+    data[index] = {
+        name : req.body.name,
+        height : parseInt (req.body.height),
+        weight : parseInt (req.body.weight),
+        birthdate : req.body.birthdate,
+        married : req.body.married == 'true'
+
+    };
+    saveData(data);
+    res.redirect('/');
 });
 
 // DELETE
 app.get('/delete/:index', (req, res) => {
-  const data = loadData();
-  data.splice(req.params.index, 1);
-  saveData(data);
-  res.redirect('/');
+    const data = loadData();
+    const index = parseInt(req.params.index) - 1;
+   if (index >= 0 && index < data.length){
+    data.splice(index, 1);
+    saveData(data);
+   }
+
+    res.redirect('/');
 });
 
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
