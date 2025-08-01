68063613bd864c831bd3d4aa857202f7fe68e118
kemoderate <fahminomana@gmail.com>
2025-08-01 15:58:48 +0700

    data sudah muncul

--- C20/db.sql
+++ C20/db.sql
@@ -7,14 +7,14 @@ CREATE TABLE data (
     married boolean default false
 );
 
-"name": "Himawan",
-    "height": 168,
-    "weight": 80.52,
-    "birthdate": "1991-03-12",
-    "married": false
+-- "name": "Himawan",
+--     "height": 168,
+--     "weight": 80.52,
+--     "birthdate": "1991-03-12",
+--     "married": false
 
 INSERT INTO data (name, height, weight, birthdate , married) VALUES
-('Hilmi', 185, 59.6, 20),
+('Hilmi', 185, 59.6, "2001-12-13","married : false"),
 ('MK002', 'Basis Data', 3),
 ('MK003', 'Data Mining', 4),
 ('MK004', 'Statistik', 3);
