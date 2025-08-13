08d99c1509a2ea4453ee2a02a9ce71c79424328d
kemoderate <fahminomana@gmail.com>
2025-08-13 11:44:18 +0700

    login dan authentikasi beres

--- C21/routes/index.js
+++ C21/routes/index.js
@@ -7,15 +7,105 @@ const saltRounds = 10;
 
 
 /* GET home page. */
-router.get('/',function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', async (req, res, next) => {
+  try {
+    let { id, string, startdate, enddate, boolean, page, op } = req.query;
+    page = parseInt(page) || 1;
+    const limit = 5;
+    const offset = (page - 1) * limit;
 
-router.get('/register', function (req, res) {
+
+    let filters = [];
+    let params = [];
+
+    if (id) {
+      filters.push(`id = $${params.length + 1}`);
+      params.push(id);
+    }
+
+    if (string) {
+      filters.push('title LIKE ?');
+      params.push(`%${string}%`);
+    }
+    if (startdate && enddate) {
+      filters.push('deadline BETWEEN ? AND ?');
+      params.push(startdate, enddate);
+    } else if (startdate) {
+      filters.push('deadline >= ?');
+      params.push(startdate);
+    } else if (enddate) {
+      filters.push('deadline <= ?');
+      params.push(enddate);
+    }
+
+    // Perbaikan: ubah string ke boolean numerik
+    if (boolean === 'true') {
+      filters.push('complete = ?');
+      params.push(1);
+    } else if (boolean === 'false') {
+      filters.push('complete = ?');
+      params.push(0);
+    }
+
+    // console.log(boolean,'ini boolean')
+
+    const operator = (op && (op === ' OR ' || op === ' AND ')) ? ` ${op} ` : ' AND ';
+    let where = filters.length ? `WHERE ${filters.join(operator)}` : '';
+
+    let countQuery = `SELECT COUNT(*) as count FROM todos ${where}`;
+    let dataQuery = `SELECT * FROM todos ${where} LIMIT = $1 OFFSET = $2`;
+
+
+    pool.query(countQuery, params, (err, countResult) => {
+      if (err) {
+        console.error('Count Error:', err);
+        return res.status(500).send('Internal Server Error');
+      }
+      let totalRows = countResult.count;
+      let totalPages = Math.ceil(totalRows / limit);
+
+      const queryWithoutPage = { ...req.query };
+      delete queryWithoutPage.page;
+
+      // Buat URL base untuk pagination
+      const fullQuery = { ...req.query, page: undefined };
+      const queryString = new URLSearchParams(queryWithoutPage).toString();
+      const baseUrl = `/?${queryString}`;
+
+      const url = req.url == '/' ? '/?page=1' : req.url
+
+
+      pool.query(dataQuery, [...params, limit, offset], (err, rows) => {
+        if (err) {
+          console.error('Data Fetch Error:', err);
+          return res.status(500).send('Internal Server Error');
+        }
+        res.render('index', {
+          title: 'SQLite BREAD (Browse,Read,Edit,Add,Delete) and Pagination',
+          data: rows,
+          query: req.query,
+          page,
+          totalPages,
+          url: baseUrl,
+        })
+      });
+    });
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).send('Internal Server Error');
+  }
+  });
+
+  
+
+
+
+
+    router.get('/register', function (req, res) {
   res.render('register', { title: 'Register User' })
 })
 
-router.post('/register', async (req, res) => {
+    router.post('/register', async (req, res) => {
   try {
     const { email, password, repassword } = req.body;
     if (!email || !password) {
@@ -23,7 +113,7 @@ router.post('/register', async (req, res) => {
       return res.redirect('/register');
     }
     if (repassword !== password) {
-     req.flash('error_msg', 'password tidak sama');
+      req.flash('error_msg', 'password tidak sama');
       return res.redirect('/register');
     }
     const plainPassword = password;
@@ -46,7 +136,7 @@ router.post('/register', async (req, res) => {
 
 
 router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
+  if (req.session.user) {
     return res.redirect('/');
   }
   res.render('login', { title: 'Login User' });
@@ -66,17 +156,18 @@ router.post('/login', async (req, res) => {
     );
 
     if (result.rows.length === 0) {
-       req.flash('error_msg', 'invalid');
-       return res.redirect('/login');
+      req.flash('error_msg', 'email is invalid');
+      return res.redirect('/login');
     }
     const user = result.rows[0];
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-       req.flash('error_msg', 'invalid');
-       return res.redirect('/login');
+      req.flash('error_msg', 'password is invalid');
+      return res.redirect('/login');
     } else {
-      req.session.userId = email;
+      req.session.user = { id: user.id, email: user.email };
       req.session.loggedIn = true;
+      req.flash('success_msg', 'Login Berhasil')
       res.redirect('/')
     }
   } catch (error) {
