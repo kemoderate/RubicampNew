800cae8d4fc076f1161ee49e5cc29caf4d5f8718
kemoderate <fahminomana@gmail.com>
2025-09-12 18:29:01 +0700

    datatables muncul fix

--- POS-APP/routes/users.js
+++ POS-APP/routes/users.js
@@ -1,18 +1,29 @@
 var express = require('express');
+const db = require('../db')
 
 
 
 /* GET home page. */
-module.exports = (requireLogin) => {
+module.exports = (requireLogin,db) => {
 
   const router = express.Router();
 
-  router.get('/', requireLogin, (req, res) => {
-    res.render('users', {
-      title: 'User',
-      user: req.session.user,
-      layout: 'layout'
-    });
+  router.get('/', requireLogin, async (req, res) => {
+    try {
+      const result = await db.query('SELECT userid, name, email, role FROM users ORDER BY userid ASC');
+      const users = result.rows;
+
+      // render EJS dan kirim data users
+      res.render('users', {
+        title: 'Users',
+        users,
+        user: req.session.user,
+        layout: 'layout'
+      });
+    } catch (err) {
+      console.error(err);
+      res.send('Error ' + err);
+    }
   });
 
 
