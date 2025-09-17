d87d7981650f46d18bb88d93f25de37900dc34b8
kemoderate <fahminomana@gmail.com>
2025-09-17 15:27:29 +0700

    fix tfoot datatables

--- POS-APP/routes/users.js
+++ POS-APP/routes/users.js
@@ -37,6 +37,10 @@ module.exports = (requireLogin, db) => {
   })
   router.post('/add', requireLogin, async (req, res) => {
     const { name, password, email, role } = req.body
+
+    if (!email || !name || !password || !role) {
+      return res.status(400).send('Semua field wajib diisi!');
+    }
     try {
       await db.query(
         'INSERT INTO users (name,password,email,role) VALUES ($1,$2,$3,$4)',
@@ -49,6 +53,49 @@ module.exports = (requireLogin, db) => {
       res.status(500).send('Failed to add User')
     }
   });
+
+  router.get('/edit/:id', requireLogin, async (req, res) => {
+    const { id } = req.params
+    try {
+      const { rows } = await db.query('SELECT * FROM users WHERE userid = $1', [id]);
+      if (rows.length === 0) return res.status(404).send('User Not Found')
+      res.render('user-form', {
+        title: 'Edit user',
+        action: `/users/edit/${id}`,
+        userData: rows[0],
+        user: req.session.user,
+      })
+    } catch (err) {
+      console.error(err)
+      res.status(500).send('Error Showing Edit Form')
+    }
+  });
+
+  router.post('edit/id', requireLogin, async (req, res) => {
+    const { id } = req.params
+    const { name, email, password, role } = req.body
+    try {
+      await db.query('UPDATE users SET name=$1, email=$2, role=$3 WHERE userid=$4',
+        [name, email, password, role]
+      )
+      res.redirect('/users')
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Failed to update user')
+    }
+  })
+
+  router.get('/delete/:id', requireLogin, async (req, res) => {
+    const { id } = req.params
+    try {
+      await db.query('DELETE FROM users WHERE userid = $1', [id])
+      res.redirect('/users')
+    } catch (err) {
+      console.error(err)
+      res.status(500).send('Failed to delete user')
+    }
+  })
+
 
   return router;
 }
